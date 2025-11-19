792aba564f109a6832d7213d7145b5859f4d0224
Axin Li <ali166@binghamton.edu>
2025-11-19 12:54:25 -0500

    user & auth

--- server/config/reset.js
+++ server/config/reset.js
@@ -1,12 +1,6 @@
 import { pool } from "./database.js";
 import "./dotenv.js";
 
-const testDB = async () => {
-  const testQuery = `
-        SELECT version()
-    `;
-};
-
 const createMoviesTable = async () => {
   const createTableQuery = `
         CREATE TABLE IF NOT EXISTS Movies (
@@ -62,7 +56,64 @@ const createMovieGenresTable = async () => {
   }
 };
 
+const createUsersTable = async () => {
+    const createTableQuery = ` CREATE TABLE IF NOT EXISTS users (
+        id serial PRIMARY KEY,
+        githubid int NOT NULL,
+        username varchar(200) NOT NULL,
+        avatarurl varchar(500),
+        accesstoken varchar(500) NOT NULL
+    );
+`;  try {
+        await pool.query(createTableQuery);
+        console.log("Movies table created successfully");
+    } catch (error) {
+        console.log("Error creating movies table" + error);
+    };
+
+};
+
+const createUserReviewTable = async () => {
+    const createTableQuery = ` CREATE TABLE IF NOT EXISTS user_reviews (
+                id serial PRIMARY KEY,
+                user_id int NOT NULL,
+                movie_id int NOT NULL,
+                rating int,
+                review_text TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id),
+                FOREIGN KEY (movie_id) REFERENCES movies(id)
+            );
+`;  try {
+        await pool.query(createTableQuery);
+        console.log("Movies table created successfully");
+    } catch (error) {
+        console.log("Error creating movies table" + error);
+    };
+}    
+
+
+const createUserMovieShelfTable = async() => {
+    const createTableQuery = `CREATE TABLE IF NOT EXISTS user_movie_shelf (
+            id serial PRIMARY KEY,
+            user_id int NOT NULL,
+            movie_id int NOT NULL,
+            status TEXT,
+            watchlist_priority int,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+`;  try {
+        await pool.query(createTableQuery);
+        console.log("Movies table created successfully");
+    } catch (error) {
+        console.log("Error creating movies table" + error);
+    };
+};
+
+
 await createMoviesTable();
 await createGenresTable();
 await createMovieGenresTable();
-export default testDB;
+await createUsersTable();
+await createUserReviewTable();
+await createUserMovieShelfTable();
